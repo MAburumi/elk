37a4f89de1d9f02532c1f7d898e80197a341b367
MAburumi <63251802+MAburumi@users.noreply.github.com>
2022-09-20 00:53:41 +0300

    Update fixNetwork.sh

--- fixNetwork.sh
+++ fixNetwork.sh
@@ -1,4 +1,7 @@
 apt install speedtest-cli
+sleep 5
 sudo lshw -class network -short && nmcli devices status
+sleep 5
 apt install network-manager
+sleep 20
 sudo nano /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
